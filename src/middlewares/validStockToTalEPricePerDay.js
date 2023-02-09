a1624e6977458ad2dd3266a915a73d9e85eee4f9
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-02-09 17:15:22 -0300

    refactory: POST games

--- src/middlewares/validStockToTalEPricePerDay.js
+++ src/middlewares/validStockToTalEPricePerDay.js
@@ -2,7 +2,7 @@
 export function validStockToTalEPricePerDay(req, res, next){
     const {stockTotal, pricePerDay} = req.body;
 
-    if(stockTotal === 0 || pricePerDay === 0){
+    if(stockTotal <= 0 || pricePerDay <= 0){
         res.sendStatus(400);
         return;
     };
