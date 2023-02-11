ff9b17d7137bb3db043ae66691347db336c7b046
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-02-11 20:52:16 -0300

    refactory8: POST Rentals Insert

--- src/routes/rentalsRoutes.js
+++ src/routes/rentalsRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { postRentals } from "../controllers/rentalsControllers.js";
-// import { validateSchema } from "../middlewares/validSchema.js";
-// import { rentalSchemas } from "../models/schemas.js";
-// import { validBodyRentals } from "../middlewares/validBodyRentals.js";
+import { validateSchema } from "../middlewares/validSchema.js";
+import { rentalSchemas } from "../models/schemas.js";
+import { validBodyRentals } from "../middlewares/validBodyRentals.js";
 
 const router = Router();
 
 router.get("/rentals");
-router.post("/rentals", postRentals);
+router.post("/rentals", validateSchema(rentalSchemas), validBodyRentals ,postRentals);
 router.post("/rentals/:id/return");
 router.delete("/rentals/:id");
 
