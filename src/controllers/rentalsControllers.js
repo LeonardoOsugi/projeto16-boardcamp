8db2ebee077ab1a76d0624996dbbb9bb438b73b7
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-02-11 19:54:29 -0300

    refactory4: POST Rentals Insert

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -7,15 +7,23 @@ export async function postRentals(req, res){
     try{
         const precoGame = await db.query(`SELECT ("pricePerDay") FROM games WHERE id = $1`, [gameId]);
 
-        const stockExist = await db.query(`SELECT ("stockTotal") FROM games WHERE id = $1`, [gameId]);
-
-        if(stockExist.rows[0].stockTotal === 0){
-            return res.status(400).send({message: "Não temos mais esse produto em stock"});
-        }
-
-        await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7);`,[customerId, gameId, dayjs().locale("pt").format("YYYY-MM-DD"), daysRented, null, daysRented*precoGame.rows[0].pricePerDay, null]);
-
-        await db.query(`UPDATE games SET "stockTotal" = $1 WHERE id = $2`, [stockExist.rows[0].stockTotal - 1, gameId]);
+        await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "originalPrice")
+    SELECT $1, $2, $3, $4,  $5 * (SELECT "pricePerDay" FROM games WHERE id = $6)
+    WHERE EXISTS (
+        SELECT * FROM customers
+        WHERE id = $7
+    ) AND EXISTS (
+     SELECT * FROM games
+     WHERE id = $8
+    ) AND (
+        SELECT "stockTotal"
+        FROM games
+        WHERE id = $9
+    ) > (
+        SELECT COUNT(*)
+        FROM rentals
+        WHERE "gameId" = $8 AND "returnDate" ISNULL
+    );`,[customerId, gameId, new Date(), daysRented, daysRented,gameId, customerId, gameId, gameId, gameId]);
 
         res.sendStatus(201);
 
