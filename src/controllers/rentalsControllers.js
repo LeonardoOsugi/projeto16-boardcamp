fdb7c9b4fcc4810c7b4d8743e790452f91433b89
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-02-11 23:02:44 -0300

    refactory12: POST Rentals Insert

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -6,14 +6,14 @@ export async function postRentals(req, res){
 
     try{
         const stockGame = await db.query(`SELECT "stockTotal" FROM games WHERE id = $1`, [gameId]);
-        const stocks = await db.query(`SELECT COUNT(*) FROM rentals WHERE "gameId" = $1 AND "returnDate" ISNULL`, [gameId]);
+        const stocks = await db.query(`SELECT COUNT(*) FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL`, [gameId]);
 
         if(stockGame.rows[0].stockTotal < stocks.rows[0].count){
             return res.status(400).send({message: "é esse mesmo ;)"});
         }
 
         await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "originalPrice") SELECT $1, $2, $3, $4,  $5 * (SELECT "pricePerDay" FROM games WHERE id = $6) WHERE EXISTS (
-        SELECT * FROM customers WHERE id = $7) AND EXISTS ( SELECT * FROM games WHERE id = $8 ) AND ( SELECT "stockTotal" FROM games WHERE id = $9 ) > ( SELECT COUNT(*) FROM rentals WHERE "gameId" = $10 AND "returnDate" ISNULL )`,[customerId, gameId, dayjs().locale("pt").format("YYYY-MM-DD"), daysRented, daysRented,gameId, customerId, gameId, gameId, gameId]);
+        SELECT * FROM customers WHERE id = $7) AND EXISTS ( SELECT * FROM games WHERE id = $8 ) AND ( SELECT "stockTotal" FROM games WHERE id = $9 ) > ( SELECT COUNT(*) FROM rentals WHERE "gameId" = $10 AND "returnDate" IS NULL )`,[customerId, gameId, dayjs().locale("pt").format("YYYY-MM-DD"), daysRented, daysRented,gameId, customerId, gameId, gameId, gameId]);
 
         res.sendStatus(201);
 
